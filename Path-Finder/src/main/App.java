fc717d7773b023cb30bdd0f796d9db21fa4c6cbc
Markus Heilmann <MHeilmann@gmx.at>
2017-05-16 13:39:31 +0200

    Created a window with ImagePanels and a Menu Bar

--- Path-Finder/src/main/App.java
+++ Path-Finder/src/main/App.java
@@ -1,10 +1,75 @@
 package main;
 
-public class App {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-	public static void main(String[] args) {
-		// @TODO Auto-generated method stub
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
+public class App extends JFrame {
+
+	private static final long serialVersionUID = 192323L;
+	public static final Dimension PANEL_SIZES = new Dimension(600,600);
+	private ImagePanel sourceImage;
+	private ImagePanel targetImage;
+	
+	public App() {
+		super("Path Finder");
+		initUI();
 	}
+	
+	private void initUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setLayout(new BorderLayout());
 
+		JMenuBar bar = new JMenuBar();
+		JMenu menu = new JMenu("Menü");
+		
+		JMenuItem item = new JMenuItem("Load");
+		menu.add(item);
+		
+		item = new JMenuItem("ThresoldFilter");
+		menu.add(item);
+		
+		item = new JMenuItem("Start");
+		menu.add(item);
+		
+		menu.addSeparator();
+		
+		item = new JMenuItem("Exit");
+		menu.add(item);
+		bar.add(menu);
+		
+		JPanel imagingPanel = new JPanel();
+		imagingPanel.setLayout(new BoxLayout(imagingPanel, BoxLayout.Y_AXIS));
+		sourceImage = new ImagePanel(PANEL_SIZES);
+		targetImage = new ImagePanel(PANEL_SIZES);
+		
+		setJMenuBar(bar);
+		
+		Box b = Box.createHorizontalBox();
+		b.add(sourceImage);
+		b.add(Box.createHorizontalStrut(5));
+		b.add(targetImage);
+		imagingPanel.add(b);
+		
+		getContentPane().add(imagingPanel, BorderLayout.CENTER);
+	}
+	
+	private void launch() {
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		App window = new App();
+		window.launch();
+	}
 }
