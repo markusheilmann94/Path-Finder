c486e82bffd3935eade2f88dcc20957056d05de2
Markus Heilmann <MHeilmann@gmx.at>
2017-05-16 14:09:26 +0200

    Added loaded Function

--- Path-Finder/src/main/App.java
+++ Path-Finder/src/main/App.java
@@ -2,14 +2,26 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
 
 public class App extends JFrame {
 
@@ -17,10 +29,12 @@ public class App extends JFrame {
 	public static final Dimension PANEL_SIZES = new Dimension(600,600);
 	private ImagePanel sourceImage;
 	private ImagePanel targetImage;
+	private ImageFilterCtrl ctrl;
 	
 	public App() {
 		super("Path Finder");
 		initUI();
+		ctrl = new ImageFilterCtrl(sourceImage, targetImage);
 	}
 	
 	private void initUI() {
@@ -31,10 +45,35 @@ public class App extends JFrame {
 		JMenuBar bar = new JMenuBar();
 		JMenu menu = new JMenu("Menü");
 		
-		JMenuItem item = new JMenuItem("Load");
-		menu.add(item);
+		JMenuItem load = new JMenuItem("Load");
+		final JFrame tmpframe = this;
+		load.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JFileChooser fc = new JFileChooser();
+				fc.setFileFilter(new FileFilter() {
+					@Override
+					public String getDescription() {
+						return "JPEG/PNG Images";
+					}
+					@Override
+					public boolean accept(File f) {
+						String n = f.getName().toLowerCase();
+						return n.endsWith(".jpg") || n.endsWith(".png");
+					}
+				});
+				fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+				if(fc.showOpenDialog(tmpframe) == JFileChooser.APPROVE_OPTION) {
+					File f = fc.getSelectedFile();
+					BufferedImage img = loadImage(f);
+					ctrl.loadImage(img);
+				}
+			}
+		});
+		menu.add(load);
 		
-		item = new JMenuItem("ThresoldFilter");
+		JMenuItem item = new JMenuItem("ThresoldFilter");
 		menu.add(item);
 		
 		item = new JMenuItem("Start");
@@ -60,6 +99,28 @@ public class App extends JFrame {
 		imagingPanel.add(b);
 		
 		getContentPane().add(imagingPanel, BorderLayout.CENTER);
+	}
+	
+	public BufferedImage loadImage(InputStream stream) {
+		try {
+			return ImageIO.read(stream);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		}
+		return null;
+	}
+	
+	public BufferedImage loadImage(File f) {
+		try {
+			return loadImage(new FileInputStream(f));
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage());
+		}
+		return null;
+	}
+	
+	public BufferedImage loadImage(String fileName) {
+		return loadImage(new File(fileName));
 	}
 	
 	private void launch() {
---
+++ Path-Finder/src/main/ImageFilterCtrl.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.Properties;
+
+/**
+ * Controls the Image Filter app
+ * 
+ * @author Tom
+ */
+public class ImageFilterCtrl {
+
+	private ImagePanel source, target;
+	
+	private BufferedImage scaled;
+	
+	private Image filtered;
+	
+	/** Creates new controller */
+	public ImageFilterCtrl(ImagePanel source, ImagePanel target) {
+		this.source = source;
+		this.target = target;
+	}
+	
+	/** Loads a new Image into this controller and displays it in the source panel */
+	public void loadImage(BufferedImage img) {
+		int type = BufferedImage.TYPE_INT_ARGB;
+
+		float s = 1.0f;
+		float wFactor = (float) App.PANEL_SIZES.width / (float) img.getWidth();
+		float hFactor = (float) App.PANEL_SIZES.height / (float) img.getHeight();
+		if (wFactor < hFactor) {
+			s = wFactor;
+		} else {
+			s = hFactor;
+		}
+
+		int w = (int) (img.getWidth() * s), h = (int) (img.getHeight() * s);
+
+		Image tmp = img.getScaledInstance(w, h, 90);
+		scaled = new BufferedImage(w, h, type);
+		Graphics2D g = scaled.createGraphics();
+		g.drawImage(tmp, 0, 0, null);
+		g.dispose();
+		//scaled = Tools.scale(img, App.PANEL_SIZES.width, App.PANEL_SIZES.height, 90);
+		source.loadImage(scaled);
+		target.loadImage(scaled);
+	}
+	
+	/** Applies a filter to the loaded source image and shows the generated image in the target panel *//*
+	public void applyFilter(FilterInterface f, Properties p) {
+		filtered = f.runFilter(scaled, p);
+		target.loadImage(filtered);
+	}
+
+	*//** Sets filtered image as source image in order to apply further filters *//*
+	public void setTargetAsSource() {
+		scaled = target.getBufferedImage();
+		source.loadImage(scaled);
+	}*/
+	
+}
