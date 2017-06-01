c3b71da63a7bd858594fa8bd43f798836dc7ae14
mmsue <Georg2@MSI2>
2017-06-01 17:52:56 +0200

    commit new threshold

--- Path-Finder/src/main/filter/Threshold.java
+++ Path-Finder/src/main/filter/Threshold.java
@@ -4,6 +4,8 @@ package main.filter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+import main.PathPoint;
+
 
 public class Threshold {
 
@@ -19,18 +21,65 @@ public class Threshold {
         int red = 0;
         int green = 0;
         int blue = 0;
+        boolean nearWalkable;
+       
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
+            	
+            	
+            	nearWalkable = false;           	
                 Color color = new Color(img.getRGB(x, y));
                 red = color.getRed();
                 green = color.getGreen();
                 blue = color.getBlue();
                 if ((red + green + blue) / 3 < requiredTH) {
-                    convertedImage.setRGB(x,y,0); //Schwarz
+                
+					for(int pY = 0 ; pY < 3 ; pY++ ) {
+						for(int pX = 0 ; pX < 3 ; pX++ ) {
+							
+							if( ( x + pX -1 ) >= 0 && ( x + pX -1 ) < width && ( y + pY -1 ) >= 0 && ( y + pY -1 ) < height && !( ( pX == 1 ) && ( pY == 1 ) ) ) {
+							
+								
+			                color = new Color(img.getRGB( ( x + pX -1 ) , ( y + pY -1 ) ) );
+			                red = color.getRed();
+			                green = color.getGreen();
+			                blue = color.getBlue();
+			                
+			                	if ((red + green + blue) / 3 > requiredTH) {
+			                	
+			                	nearWalkable = true; 
+			               	
+			                	}
+
+							}
+		
+						}
+					}	
+					
+                	if( nearWalkable ) {
+                		
+                		convertedImage.setRGB(x,y, 0xFF000000 ); //Schwarz
+                	                		
+                	}
+                	else{
+                		
+                		
+                		convertedImage.setRGB(x,y, img.getRGB(x, y) );
+                		
+                	}
+					
+					
+					
                 } else {
-                    convertedImage.setRGB(x,y,rgbWhite); //Weiß
+                                	
+                	convertedImage.setRGB(x,y,rgbWhite); //Weiß
                 }
+                
+                
+                
+                
+                
             }
         }
 
